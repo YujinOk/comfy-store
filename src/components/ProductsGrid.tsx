40009aedb099eb8132a270d8cf5f3d5cb062d93b
Yujin Ok <yujinok63@gmail.com>
2024-08-19 18:26:00 +0900

    fixed the paths for shadcn

--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -1,4 +1,33 @@
+import { Link, useLoaderData } from "react-router-dom";
+import { Card, CardContent } from "./ui/card";
+import { formatAsDollars, type ProductsResponse } from "@/utils";
+
 const ProductsGrid = () => {
-  return <div>ProductsGrid</div>;
+  const { data: products } = useLoaderData() as ProductsResponse;
+  return (
+    <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+      {products.map((product) => {
+        const { title, price, image } = product.attributes;
+        const dollarAmount = formatAsDollars(price);
+        return (
+          <Link to={`/products/${product.id}`} key={product.id}>
+            <Card>
+              <CardContent className="p-4">
+                <img
+                  src={image}
+                  alt={title}
+                  className="rounded-md h-64 md:h-48 w-full object-cover"
+                />
+                <div className="mt-4 text-center">
+                  <h2 className="text-xl font-semibold capitalize">{title}</h2>
+                  <p className="text-primary font-light mt-2">{dollarAmount}</p>
+                </div>
+              </CardContent>
+            </Card>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 export default ProductsGrid;
